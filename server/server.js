54d761fdf86f00e4d0d97c25f1ceeab4d89d7256
Russ <shtooney@yahoo.com>
2019-02-15 23:50:01 -0500

    GETs added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 let {mongoose} = require('./db/mongoose')
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
@@ -30,6 +31,30 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+    
+    let id = req.params.id;
+
+    
+    //validate ID = stop and give back 404 and send back empty body
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    } else {
+        Todo.findById(id).then((todo) => {
+            if(!todo){
+                res.status(404).send();
+            } else {
+                //console.log('User by Id:', JSON.stringify(user, undefined, 2));
+                res.send({todo});
+            }            
+        }).catch((e) => res.status(400).send());
+        
+    }
+    
+
 
 });
 
