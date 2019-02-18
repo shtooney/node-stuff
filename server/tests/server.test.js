18b98048c10b707469d3ee34a83dd1eb072f584f
Russ <shtooney@yahoo.com>
2019-02-18 00:13:28 -0500

    added further tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -320,4 +320,30 @@ describe('POST /todos', () => {
 
     });
 
+    describe('DELETE /users/me/token', () => {
+
+        it('should remove auth token on logout', (done) => {
+            //empty out token & ensure tokens array is 0 for test + get 200 back
+            //then find User in DB and verify that tokens array is length of 0
+            
+            request(app)
+                .delete('/users/me/token')
+                .set('x-auth', users[0].tokens[0].token)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) {
+                        return done(err);
+                    }
+
+                    User.findById(users[0]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done()
+                    })
+                    .catch((e) => done(e));
+                });
+            
+        });
+
+    });
+
 });
